f45e7d09a285fc0fa29669d5689a7f133f65c3c8
CloseGillter47 <kuroko.link@aliyun.com>
2022-01-21 14:26:23 +0800

    3 files modified

--- src/utils/fetch/useAxios.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react';
-import { useFetchState } from './axios';
-import { AxiosRequestFn } from './index';
-
-export default function useAxios<T>(fetchFn: AxiosRequestFn) {
-  const [{ request }] = useFetchState();
-
-  const run = useCallback(async () => {}, []);
-}
